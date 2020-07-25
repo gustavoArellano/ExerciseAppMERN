bfbd1b62c06dd555e1998a1c4fabc8afbf11563d
Gustavo Arellano <gustavo813arellano@gmail.com>
2020-07-24 22:34:54 -0700

    updates for heroku

---
+++ frontend/build/precache-manifest.282e308d3a239963c400715f47dc7a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa4695e9e412a2c73db02538c38e985",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1123e648e9498a340b81",
+    "url": "/static/css/2.a73d7da8.chunk.css"
+  },
+  {
+    "revision": "1123e648e9498a340b81",
+    "url": "/static/js/2.efbdb819.chunk.js"
+  },
+  {
+    "revision": "c170058f7137030f04c4",
+    "url": "/static/js/main.f8f7c87f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+// BODY PARSER NOW AVAILALBE WITHIN EXPRESS, NO LONG NEED TO REQUIRE
+// const bodyParser = require('body-parser');
+const cors = require('cors');
+// MONGOOSE IS REQUIRED TO HELP CONNECT THE DATABASE TO THE SERVER
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+// app.use(bodyParser.json());
+
+// GRAPPING THE ATLAS_URI FROM ENV FILE CONTAINING KEY
+const uri = process.env.ATLAS_URI;
+// CONNECTS THE DATABASE, ALONG WITH PARSER AND INDEX AS TRUE
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+// ESTABLISHES CONNECTION, NOTE ALWAYS CHECK IP IS UPDATED IN THE MONGO SITE
+const connection = mongoose.connection; 
+connection.once('open', () => {
+    console.log("MongoDB databse connection established succcesfully");
+});
+
+// ONCE MODELS ARE CREATED, NEED TO REQUIRE THEM HERE IN THE SERVER
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+// WHEN SOMEONE GOES TO "/EXERCISES" OR "/USERS" URL IT WILL LOAD EVERYTING THATS IN THE ROUTERS
+// AFTER REQUIREING MODELS, TELL SERVER TO USE THEM
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+// WHEN IN DEPLOYING, ADD THE BELOW
+if (process.env.NODE_ENV === 'production') {
+    app.use((express.static('build')))
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
